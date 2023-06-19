e8f075295bf5ffe31268b5454769bc1abb9eaca0
Zenas Kwabi <zensezeus16@gmail.com>
2023-06-19 16:46:32 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - entry place
+*return : 0(valid)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Return: 0 (valid)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
